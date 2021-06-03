5f5f10ce089e4863c9cd26f42f1f7191c0bd3e4e
C1318879 Arthur Neves Monteiro <c1318879@interno.bb.com.br>
2021-06-03 11:30:00 -0300

    using next

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
+const path = '/api'
 
-app.get('/api', (request, response) => {
+app.use((req, res, next) => {
+    console.log('First call..')
+    next()
+})
+
+app.get(path, (request, response, next) => {
     response.json([
         {"name": "Arthur", "job": "Developer"},
-        {"name": "John", "job": "Medic"}
+        {"name": "John", "job": "Medic"},
+        {"name": "Mary", "job": "CEO"}
     ])
-
-    // response.json({
-    //     "name": "Arthur",
-    //     "job": "Developer"
-    // })
-
+    next()
     //response.send('<h1>Server Running</h1>')
+})
+
+app.use((req, res) => {
+    console.log('Last call..')
 })
 
 app.listen(3000, () => {
